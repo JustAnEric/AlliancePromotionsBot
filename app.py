847183e33a8b9c0f8dd3dd8abd089fb57da8bd8f
JustAnEric <ericmuzyk@icloud.com>
2023-05-30 17:31:14 +1000

    added: more

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
 from discord.ext import commands
 import os, json, discord, sys
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
-@bot.command(name="adwarn")
---
+++ structure/cogs/load.py
@@ -0,0 +1,7 @@
+import main_server_bot
+import staff_server_bot
+
+cogs = [
+    main_server_bot,
+    staff_server_bot
+]
