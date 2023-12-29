db6c51887adc6b5f5d3addecb7b05cb53e1b3aa8
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-12-29 17:21:55 +1100

    Update staff_server_bot.py

--- structure/cogs/staff_server_bot.py
+++ structure/cogs/staff_server_bot.py
@@ -13,11 +13,11 @@ class SSCog(commands.Cog):
             if role.is_assignable():
                 await user.add_roles(role)
                 return await interaction.followup.send(content=f"Promoted **{user.name}** to **{role.name}**")
-            else: return await interaction.followup.send(content=f"Unfortunately, I am unable to promote **{user.name}** to **{role.name}** because of my ***sufficient permissions***.")
+            else: return await interaction.followup.send(content=f"Unfortunately, I am unable to promote **{user.name}** to **{role.name}** because of my ***insufficient permissions***.")
         
         @promote.error
         async def _promote_error(interaction:discord.Interaction, error):
-            return await interaction.followup.send(f"You are not allowed to promote because of your **sufficient permissions**.")
+            return await interaction.followup.send(f"You are not allowed to promote because of your **insufficient permissions**.")
         
         @bot.tree.command(name="fire", description="Fire someone from the staff team. This removes all roles and rights from the user.")
         @has_permissions(manage_roles=True)
@@ -29,7 +29,7 @@ class SSCog(commands.Cog):
         
         @fire.error
         async def _fire_error(interaction:discord.Interaction, error):
-            return await interaction.followup.send(f"You are not allowed to fire because of your **sufficient permissions**.")
+            return await interaction.followup.send(f"You are not allowed to fire because of your **insufficient permissions**.")
         
         @bot.tree.command(name="demote", description="Demotes a user from a specified role. The bot will remove the highest role if no role was specified.")
         @has_permissions(manage_roles=True)
@@ -45,7 +45,7 @@ class SSCog(commands.Cog):
             
         @demote.error
         async def _demote_error(interaction:discord.Interaction, error):
-            return await interaction.followup.send(f"You are not allowed to demote because of your **sufficient permissions**.")
+            return await interaction.followup.send(f"You are not allowed to demote because of your **insufficient permissions**.")
 
         @bot.command(name="promote", brief="Promote user to a specific role.")
         @has_permissions(manage_roles=True)
@@ -54,11 +54,11 @@ class SSCog(commands.Cog):
             if role.is_assignable():
                 await user.add_roles(role)
                 return await default.edit(content=f"Promoted **{user.name}** to **{role.name}**")
-            else: return await default.edit(content=f"Unfortunately, I am unable to promote **{user.name}** to **{role.name}** because of my ***sufficient permissions***.")
+            else: return await default.edit(content=f"Unfortunately, I am unable to promote **{user.name}** to **{role.name}** because of my ***insufficient permissions***.")
         
         @raw_promote.error
         async def _raw_promote_error(ctx:commands.Context, error):
-            return await ctx.send(f"You are not allowed to promote because of your **sufficient permissions**.")
+            return await ctx.send(f"You are not allowed to promote because of your **insufficient permissions**.")
         
         @bot.command(name="fire", brief="Fire someone from the staff team. This removes all roles and rights from the user.")
         @has_permissions(manage_roles=True)
@@ -70,7 +70,7 @@ class SSCog(commands.Cog):
         
         @raw_fire.error
         async def _raw_fire_error(ctx:commands.Context, error):
-            return await ctx.send(f"You are not allowed to fire because of your **sufficient permissions**.")
+            return await ctx.send(f"You are not allowed to fire because of your **insufficient permissions**.")
         
         @bot.command(name="demote", brief="Demotes a user from a specified role. The bot will remove the highest role if no role was specified.")
         @has_permissions(manage_roles=True)
@@ -86,4 +86,11 @@ class SSCog(commands.Cog):
             
         @raw_demote.error
         async def _raw_demote_error(ctx:commands.Context, error):
-            return await ctx.send(f"You are not allowed to demote because of your **sufficient permissions**.")
+            return await ctx.send(f"You are not allowed to demote because of your **insufficient permissions**.")
+        
+        @bot.tree.command(name="strikes", description="See the strikes a user has.")
+        async def _strikes(intr:discord.Interaction, user:discord.User):
+            send = intr.response.send_message
+            author = intr.user
+            if user.bot:
+                return await send("You are unable to see the strikes for this user as they are a robot.")
